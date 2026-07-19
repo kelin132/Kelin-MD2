f0885f024ec90bd4aa5489edd71b435469e5fb86
kelin <keithernswiss@gmail.com>
2026-07-19 11:00:37 +0200

    Update mods.js

--- plugins/owner/mods.js
+++ plugins/owner/mods.js
@@ -7,7 +7,7 @@ export default {
   usage: ".mods | .addmod @user | .removemod @user",
   aliases: ["addmod", "removemod", "modlist"],
   cooldown: 5,
-  isOwner: true,
+  isOwner: false ,
   isAdmin: false,
   isPremium: false,
   version: "1.0.0",
