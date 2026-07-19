a5e6d78be3fdac893a82e70a42c9ccdb39da029e
kelin <keithernswiss@gmail.com>
2026-07-19 11:18:31 +0200

    Update broadcast.js

--- plugins/owner/broadcast.js
+++ plugins/owner/broadcast.js
@@ -6,7 +6,7 @@ export default {
   aliases: ["bc"],
   cooldown: 30,
   isOwner: true,
-  isAdmin: true,
+  isAdmin: false,
   isPremium: false,
   version: "1.0.0",
   async run({ sock, msg, text }) {
