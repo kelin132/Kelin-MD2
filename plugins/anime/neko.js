f9a287df6998687fbc9c5f3a17804e5a195e5ed1
kelin <keithernswiss@gmail.com>
2026-07-20 00:52:58 +0200

    Update neko.js

--- plugins/anime/neko.js
+++ plugins/anime/neko.js
@@ -1,19 +1,42 @@
-import { sendReaction } from "./_helper.js";
+import axios from "axios";
 
 export default {
   name: "neko",
   aliases: ["catgirl"],
-  description: "Get a random anime neko (cat girl) image",
+  description: "Get a random anime neko image",
   category: "anime",
   usage: ".neko",
 
-  async run({ sock, msg, sender }) {
-    await sendReaction({
-      sock, msg, sender,
-      type: "neko",
-      soloCaption: `🐱 *Nyan~* Here's your neko fix for the day!\n_Don't stare too long, baka 👀_`,
-      duoCaption: (from, to) => `🐱 *${from} sent a neko to ${to}~*\n_Nyaa ehe~_`,
-      errorText: "❌ The neko escaped. Try again!",
-    });
+  async run({ sock, msg }) {
+    const jid = msg.key.remoteJid;
+
+    try {
+      const { data } = await axios.get("https://api.waifu.pics/sfw/neko");
+
+      await sock.sendMessage(
+        jid,
+        {
+          image: { url: data.url },
+          caption:
+`🐱 *Nyan~!*
+
+Here's your random anime neko!
+
+> Powered by waifu.pics`,
+        },
+        { quoted: msg }
+      );
+
+    } catch (err) {
+      console.error(err);
+
+      await sock.sendMessage(
+        jid,
+        {
+          text: "❌ The neko ran away. Try again later!",
+        },
+        { quoted: msg }
+      );
+    }
   },
 };
