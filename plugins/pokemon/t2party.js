12395334d1218df9366c29a1e850d9d4f9822f51
kelin <keithernswiss@gmail.com>
2026-08-19 07:43:51 +0200

    Update t2party.js

--- plugins/pokemon/t2party.js
+++ plugins/pokemon/t2party.js
@@ -17,20 +17,20 @@ export default {
 
     if (!args[0]) {
       return sock.sendMessage(jid, {
-        text: "Usage: *.t2party <name or slot number>*\nExample: `.t2party pikachu` or `.t2party 1`\n\nUse *.pc* to see your stored Pokémon.",
+        text: "Usage: ``.t2party <name or slot number>``\nExample: ``.t2party pikachu`` or ``.t2party 1``\n\nUse ``.pc`` to see your stored Pokémon.",
       }, { quoted: msg });
     }
 
     const trainer = await getTrainer(sender);
     if (!trainer) {
-      return sock.sendMessage(jid, { text: "❌ Start your journey first! Use *.startjourney*" }, { quoted: msg });
+      return sock.sendMessage(jid, { text: "❌ Start your journey first! Use ``.startjourney``" }, { quoted: msg });
     }
 
     // Check party size using getTrainerParty (inParty: true) for accuracy
     const party = await getTrainerParty(sender);
     if ((party || []).length >= 6) {
       return sock.sendMessage(jid, {
-        text: `❌ Your party is full! (6/6)\nUse *.t2pc <name>* to move one out first.`,
+        text: `❌ Your party is full! (``6/6``)\nUse ``.t2pc <name>`` to move one out first.`,
       }, { quoted: msg });
     }
 
@@ -55,7 +55,7 @@ export default {
 
     if (!target) {
       return sock.sendMessage(jid, {
-        text: `❌ Couldn't find *${query}* in your PC.\nUse *.pc* to see your stored Pokémon.`,
+        text: `❌ Couldn't find *${query}* in your PC.\nUse ``.pc`` to see your stored Pokémon.`,
       }, { quoted: msg });
     }
 
@@ -77,7 +77,7 @@ export default {
     await updatePokemon(target._id, { inParty: true });
 
     await sock.sendMessage(jid, {
-      text: `🎒 *${target.displayName || target.name}* was added to your party! (${(party || []).length + 1}/6)\n\nUse *.party* to view your team.`,
+      text: `🎒 *${target.displayName || target.name}* was added to your party! (``${(party || []).length + 1}/6``)\n\nUse ``.party`` to view your team.`,
     }, { quoted: msg });
   },
 };
