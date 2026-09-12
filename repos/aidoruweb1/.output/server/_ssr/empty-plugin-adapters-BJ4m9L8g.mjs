47f125224158ecd17b46682a02562b1c41624eff
kelin <keithernswiss@gmail.com>
2026-09-12 11:37:18 +0200

    Delete repos directory

--- repos/aidoruweb1/.output/server/_ssr/empty-plugin-adapters-BJ4m9L8g.mjs
+++
@@ -1,5 +0,0 @@
-//#region node_modules/.nitro/vite/services/ssr/assets/empty-plugin-adapters-BJ4m9L8g.js
-var pluginSerializationAdapters = [];
-var hasPluginAdapters = false;
-//#endregion
-export { hasPluginAdapters, pluginSerializationAdapters };
